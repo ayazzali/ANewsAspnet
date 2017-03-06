8f849e11a8a46d67e76d46382048bfc4c7485cc6
Пользователь <@WINCTRL-DRTHBOQ>
2017-03-06 22:49:45 +0300

    Add project files.

---
+++ NewsAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsAspNet.MvcApplication" Language="C#" %>
---
+++ NewsAspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsAspNet.Startup))]
+namespace NewsAspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
